61d666f82a8d933807fa73e5cf15cf98430404dd
mitaly-dev <mitalyy.dev@gmail.com>
2023-07-15 11:39:30 +0600

    [DONE]-navbar created

--- src/layout/MainLayout.tsx
+++ src/layout/MainLayout.tsx
@@ -1,10 +1,12 @@
+import Navbar from '@/components/Navbar';
 import React from 'react';
 import { Outlet } from 'react-router-dom';
 
 const MainLayout = () => {
   return (
     <div>
-      <Outlet></Outlet>
+      <Navbar />
+      <Outlet />
     </div>
   );
 };
